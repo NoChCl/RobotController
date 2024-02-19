27872063471e9fa9648dd0add29e7586cdd7a553
Noah Clark <noah.clark@kearsarge.org>
2024-02-19 09:13:16 -0500

    worked on simulation

--- robotControler.py
+++ robotControler.py
@@ -15,10 +15,25 @@ buttons=[False, False, False, False, False, False]
 slider = pygame.Surface((30,50))
 slider.fill([255,255,100])
 
-aButton1=pygame.Surface((75,75), flags=pygame.SRCALPHA)
-pygame.draw.circle(aButton1, (200, 0, 0), [75/2, 75/2], 75/2)
-aButton2=pygame.Surface((75,75), flags=pygame.SRCALPHA)
-pygame.draw.circle(aButton2, (255, 0, 0), [75/2, 75/2], 75/2)
+n4Button1=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n4Button1, (150, 0, 0), [75/2, 75/2], 75/2)
+n4Button2=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n4Button2, (255, 0, 0), [75/2, 75/2], 75/2)
+
+n3Button1=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n3Button1, (0, 100, 120), [75/2, 75/2], 75/2)
+n3Button2=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n3Button2, (0, 200, 255), [75/2, 75/2], 75/2)
+
+n2Button1=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n2Button1, (0, 100, 0), [75/2, 75/2], 75/2)
+n2Button2=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n2Button2, (0, 255, 0), [75/2, 75/2], 75/2)
+
+n1Button1=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n1Button1, (100,100,100), [75/2, 75/2], 75/2)
+n1Button2=pygame.Surface((75,75), flags=pygame.SRCALPHA)
+pygame.draw.circle(n1Button2, (255,255,0), [75/2, 75/2], 75/2)
 
 tButton1=pygame.Surface((50,50), flags=pygame.SRCALPHA)
 pygame.draw.circle(tButton1, (150, 150, 150), [25, 25], 25)
@@ -36,27 +51,49 @@ while True:
             sys.exit()
             
         if event.type == pygame.JOYAXISMOTION:
-            axes[event.axis] = event.value+1
+            axes[event.axis] = (int((event.value+1)*100))
         if event.type == pygame.JOYBUTTONDOWN:
             buttons[event.button]=True
         if event.type == pygame.JOYBUTTONUP:
             buttons[event.button]=False
+
+        
+    
+    
     
     print(axes, buttons)
     
     
+    if buttons[0]:
+        n1Button = n1Button2
+    else:
+        n1Button=n1Button1
     
+    if buttons[1]:
+        n2Button = n2Button2
+    else:
+        n2Button=n2Button1
+    
+    if buttons[2]:
+        n3Button = n3Button2
+    else:
+        n3Button=n3Button1
+    
+    if buttons[3]:
+        n4Button = n4Button2
+    else:
+        n4Button=n4Button1
     
     screen.fill([0,0,255])
 
-    screen.blit(slider, [850, 400])
-    screen.blit(aButton1, [865-75/2, 100])
-    screen.blit(slider, [650, 400])
-    screen.blit(aButton1, [665-75/2, 100])
-    screen.blit(slider, [450, 400])
-    screen.blit(aButton1, [465-75/2, 100])
-    screen.blit(slider, [250, 400])
-    screen.blit(aButton2, [265-75/2, 100])
+    screen.blit(slider, [850, 400-axes[6]])
+    screen.blit(n4Button, [865-75/2, 100])
+    screen.blit(slider, [650, 400-axes[2]])
+    screen.blit(n3Button, [665-75/2, 100])
+    screen.blit(slider, [450, 400-axes[5]])
+    screen.blit(n2Button, [465-75/2, 100])
+    screen.blit(slider, [250, 400-axes[0]])
+    screen.blit(n1Button, [265-75/2, 100])
     
     screen.blit(tButton1, [75, 200])
     screen.blit(tButton2, [75, 300])
